0f2965dbd62dee3833c9876ff4d95984ed94688e
Serenity <d.palbanese@gmail.com>
2021-05-10 20:10:09 +0200

    Reversed removal of auto update function

--- core/helpers/autoUpdater.ts
+++ core/helpers/autoUpdater.ts
@@ -19,14 +19,14 @@ const updateApp = (mainWindow: BrowserWindow) => {
     dialog.showMessageBox({
       type: 'info',
       title: 'Update available',
-      message: `A new version of Media Dashboard is available ${autoUpdater.currentVersion.version} -> ${info.version}. Do you want to download now?`,
+      message: `A new version of Media Dashboard is available ${autoUpdater.currentVersion.version} -> ${info.version}. Do you want to update now?`,
       buttons: ['Yes', 'No']
     }).then(res => {
       const buttonIndex = res.response;
       if (buttonIndex === 0) {
-        shell.openExternal('https://github.com/Daniele122898/media-dashboard/releases')
-        // autoUpdater.downloadUpdate()
-        //   .catch(e => logger.error('Update download failed', e));
+        // shell.openExternal('https://github.com/Daniele122898/media-dashboard/releases')
+        autoUpdater.downloadUpdate()
+          .catch(e => logger.error('Update download failed', e));
       }
     });
 
@@ -34,27 +34,27 @@ const updateApp = (mainWindow: BrowserWindow) => {
 
   // Listen for update download finished
   // TODO periodically check if this crap is ever fixed
-  // autoUpdater.on('update-downloaded', () => {
-  //   mainWindow.setProgressBar(-1);
-  //   dialog.showMessageBox({
-  //     type: 'info',
-  //     title: 'Update ready',
-  //     message: 'Update downloaded. Install update & restart now?',
-  //     buttons: ['Yes', 'Later']
-  //   }).then(res => {
-  //     const buttonIndex = res.response;
-  //
-  //     // install and restart
-  //     if (buttonIndex === 0) {
-  //       autoUpdater.quitAndInstall(false, true);
-  //     }
-  //   });
-  // });
-  //
-  // // Listen for download progress
-  // autoUpdater.on('download-progress', (progress, bytesPerSecond, percent) => {
-  //   mainWindow.setProgressBar(progress);
-  // });
+  autoUpdater.on('update-downloaded', () => {
+    mainWindow.setProgressBar(-1);
+    dialog.showMessageBox({
+      type: 'info',
+      title: 'Update ready',
+      message: 'Update downloaded. Install update & restart now?',
+      buttons: ['Yes', 'Later']
+    }).then(res => {
+      const buttonIndex = res.response;
+
+      // install and restart
+      if (buttonIndex === 0) {
+        autoUpdater.quitAndInstall(false, true);
+      }
+    });
+  });
+
+  // Listen for download progress
+  autoUpdater.on('download-progress', (progress, bytesPerSecond, percent) => {
+    mainWindow.setProgressBar(progress);
+  });
 };
 
 export default updateApp;
